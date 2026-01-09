50cceea24e84cf335b65c67e9e2f53d483869dd5
Isaac <isaac.keninger@gmail.com>
2026-01-09 15:51:54 -0600

    Hi from port 8080!

--- HTTPServer.java
+++ HTTPServer.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader; // This helps to create a netowrk socket on a port
-import java.net.ServerSocket; // import socket object for getting client info
-import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.Socket; // This helps to create a netowrk socket on a port
 
 
 public class HTTPServer{
@@ -10,20 +8,17 @@ public class HTTPServer{
         final ServerSocket server = new ServerSocket(8080); // This creates a network socket that accepts connections on port 8080
         System.out.println("Listening on Port 8080...");
         while (true){ // do this forever while the server is up and running
-            Socket clientSocket = server.accept(); // blocking method for connecting ot server
+            try (Socket socket = server.accept()){
+                String message = "Hi from port 8080!";
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + message;
+                socket.getOutputStream()
+                    .write(httpResponse.getBytes("UTF-8")); 
+
+            }
             // Read HTTP request from Client socket above
             // make an http response
             // send the http response back to client
             // close
-
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine(); // this line showw each of the lines from the clients sockets input stream (information about the client and connection)
-            while (!line.isEmpty()){
-                System.out.println(line);
-                line = reader.readLine();
-            }
-
         }
     }
 }
